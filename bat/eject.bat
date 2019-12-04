f1b3b4e8655297c23b7723c983abb3fddc98d437
Raymond Wu <raymond.wu@insyde.com>
2019-12-04 11:14:57 +0800

    [eject] Add -l -r command
    Signed-off-by: Raymond Wu <raymond.wu@insyde.com>

--- bat/eject.bat
+++ bat/eject.bat
@@ -1,6 +1,34 @@
 @echo off
 setlocal enabledelayedexpansion
 
-diskpart /s %~dp0\eject\listvolume
+rem [begin main]
+if "%1" == "" (
+	goto :help
+)
+
+if "%1" == "-l" (
+	diskpart /s %~dp0\eject\listvolume
+)
+
+if "%1" == "-r" (
+	if "%2" == "" (
+		goto :help
+	)
+	echo select volume  %2  > %~dp0\eject\dismount
+	echo remove all dismount >> %~dp0\eject\dismount
+	diskpart /s %~dp0\eject\dismount
+)
+goto :EOF
+rem [end main]
+
+rem [begin help]
+:help
+	echo EJECT [command]
+	echo.
+	echo -l		List all volume
+	echo -r [Num]	Safely remove
+	echo -i [Num]	Insert a usb and assign a letter
+goto :EOF
+rem [end help]
 
 endlocal
